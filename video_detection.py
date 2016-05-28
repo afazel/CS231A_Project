de0280111fc418f85b6b8df920f50ef01e8bef96
afazel <azarmehr65@gmail.com>
2016-05-27 19:31:11 -0700

    updated version of video_detection.py

--- video_detection.py
+++ video_detection.py
@@ -1,57 +1,65 @@
 import cv2
-cap = cv2.VideoCapture("/Users/azarf/Desktop/passageway_test.mp4")
+import is_pedestrian as ped
+import math
+from skimage import color
+
+cap = cv2.VideoCapture("/Users/azarf/Desktop/ped_vid_good_azar.mp4")
 #passageway_test.mp4
 #Recording-Session-840674.mp4
 ret, frame = cap.retrieve()
-#print frame[0]
-
-#Recording-Session-840674.mp4
-
-# while True:
-#     if cap.grab():
-
-#         #flag, frame = cap.retrieve()
-#         flag, frame = cap.read()
-#         print "here",frame
-#         if not flag:
-#             continue
-#         else:
-#             cv2.imshow('video', frame)
-#     if cv2.waitKey(10) == 27:
-#         break
-
 fgbg = cv2.BackgroundSubtractorMOG()
 
 while(1):
     ret, frame = cap.read()
-    #print frame
-
+    #print frame.shape
     fgmask = fgbg.apply(frame)
-    #print fgmask
-
     cv2.imshow('frame',fgmask)
     cv2.imshow('img',frame)
-    # if cv2.waitKey(10) == 27:
-    #      break
-    k = cv2.waitKey(30) & 0xff
-    if k == 5:
-    	break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    (cnts, _) = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    (contours, _) = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    for c in cnts:
-		# if the contour is too small, ignore it
-		#print cv2.contourArea(c)
-		if cv2.contourArea(c) < 100:
-			continue
+    for c in contours:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < 100:
+            continue
  
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		print x+w,y+h
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		cv2.imshow("detect",frame)
-		cv2.waitKey(1)
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (xmin, ymin, w, h) = cv2.boundingRect(c)
+        window_size = [64, 128]
+        xmax = xmin + w
+        ymax = ymin + h
+        if w < window_size[0]:
+            padding_width = window_size[0] - w
+            xmin_p = xmin-math.ceil(padding_width / 2)
+            xmax_p = xmax + math.ceil(padding_width / 2)
+            if h < window_size[1]:
+                 padding_height = window_size[1] - h
+                 ymin_p = ymin - math.ceil(padding_height / 2)
+                 ymax_p = ymax + math.ceil(padding_height / 2)
+         
+        if xmin_p < 0 or xmax_p > frame.shape[1] or ymin_p < 0 or ymax_p > frame.shape[0]:
+            continue
+
+        cv2.imshow("current image",frame[ymin_p:ymax_p, xmin_p:xmax_p])
+
+        extracted_contour = frame[ymin_p:ymax_p , xmin_p:xmax_p]
+        #print extracted_contour
+        #extracted_contour = cv2.imread(extracted_contour)
+        extracted_contour = color.rgb2gray(extracted_contour)  
+        
+        scale = 1.2
+        my_ped = ped.run_detector(extracted_contour,scale)
+        #print ped
+        if my_ped:
+            print "find ped"
+            print xmin, ymin,xmin + w, ymin + h
+            cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmin + w), int(ymin + h)), (0, 255, 0), 2)
+    cv2.imshow("detect",frame)
+    cv2.waitKey(1)
+        
 
 cap.release()
 cv2.destroyAllWindows()
